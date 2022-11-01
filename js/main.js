ed0321540db0d303480eccd95bfda96ea0caed4e
wilsonpiress <wilsonpires.js@gmail.com>
2022-11-01 10:49:33 -0300

    Manipulando o JavaScript e fazendo alguns ajustes.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", dizOi)
+const controle = document.querySelectorAll(".controle-ajuste")
 
-function dizOi() {
-    console.log("Oi")
+controle.forEach((elemento) =>{
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+})
+
+function manipulaDados(operacao) {
+    if(operacao === "-") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
 }
