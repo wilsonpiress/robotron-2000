2ed28b1f4362145794149888bbb476a8473fc830
wilsonpiress <wilsonpires.js@gmail.com>
2022-11-05 18:06:38 -0300

    Adicionei a variável peças no JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
 const controle = document.querySelectorAll("[data-controle]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade":-5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade":-20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade":-4
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade":43
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 20,
+        "energia": 0,
+        "velocidade":-2
+    }
+}
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
